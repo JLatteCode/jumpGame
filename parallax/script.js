661f89fbec74bb7d7a5a45ca6ff995a5fa1e5054
JungJae <jlee1298@my.bcit.ca>
2022-05-30 03:03:18 -0700

    change background move speed UI

--- parallax/script.js
+++ parallax/script.js
@@ -15,6 +15,16 @@ backgroundLayer4.src = 'assets/background/layer-4.png'
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'assets/background/layer-5.png'
 
+const slider = document.getElementById("slider");
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById("showGameSpeed");
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', function(e) {
+   
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
+})
+
 class Layer {
     constructor(image, speedModifier) {
         this.x = 0;
@@ -29,7 +39,7 @@ class Layer {
     update(){
         this.speed = gameSpeed * this.speedModifier;
         if (this.x <= -this.width) {
-            this.x = this.width + this.x - this.speed;
+            this.x = this.width + this.x2 - this.speed;
         }
         if (this.x2 <= -this.width) {
             this.x2 = this.width + this.x - this.speed;
@@ -43,19 +53,20 @@ class Layer {
     }
 }
 
-const layer1 = new Layer(backgroundLayer1, 0.5);
-const layer2 = new Layer(backgroundLayer2, 0.5);
-const layer3 = new Layer(backgroundLayer3, 0.5);
-const layer4 = new Layer(backgroundLayer4, 0.5);
-const layer5 = new Layer(backgroundLayer5, 0.5);
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
 
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // remove old paints, no smudging
-    layer4.update();
-    layer4.draw();
-    layer5.update();
-    layer5.draw();
+    gameObjects.forEach(e => {
+        e.update();
+        e.draw();
+    });
     requestAnimationFrame(animate);
 }
 animate();
