f7012cc6e83f16fb133d4c6d62e272d2395d3ab1
JungJae <jlee1298@my.bcit.ca>
2022-06-01 08:39:19 -0700

    npc got added and its movement

--- npcMovement/script.js
+++ npcMovement/script.js
@@ -3,15 +3,47 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
+const enemyImage = new Image();
+enemyImage.src = 'assets/npc/enemy1.png';
+
+class Enemy {
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth= 293;
+        this.spriteHeight= 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+    }
+    update(){
+        this.x += this.speed;
+        this.y += this.speed;
+
+        this.frame > 4 ? this.frame = 0 : this.frame++;
+    }
+    draw(){
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(enemyImage, this.frame* this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height); 
+    }
+};
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
 }
 
 function animate() {
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width,enemy1.height);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+ 
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
+    requestAnimationFrame(animate);
 }
 animate();
